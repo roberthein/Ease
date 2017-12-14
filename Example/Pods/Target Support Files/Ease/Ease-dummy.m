2432e39b2d8f19be72c98557dd49d95b1519b274
Robert-Hein Hooijmans <rh.hooijmans@gmail.com>
2017-12-14 18:35:44 +0100

    Pod deintegrate.

--- Example/Pods/Target Support Files/Ease/Ease-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Ease : NSObject
-@end
-@implementation PodsDummy_Ease
-@end
--- HeaderExample/Pods/Target Support Files/Pods-Ease3DSceneKit/Pods-Ease3DSceneKit-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Ease3DSceneKit : NSObject
-@end
-@implementation PodsDummy_Pods_Ease3DSceneKit
-@end
